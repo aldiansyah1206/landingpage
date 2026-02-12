78275e7f71e753614fe63b422dd1867fb42510b2
aldiansyah1206 <aldi40852@gmail.com>
2026-02-12 15:26:00 +0700

    fix api index for vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,22 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+// Set document root ke folder public
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
+// Load Composer autoload
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Handle Request
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
